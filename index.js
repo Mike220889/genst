50a9b7cf0d7385d35ccfda4e68d2c19d8bcd3c76
Mike Monteith <mike@movement.co.uk>
2015-03-19 14:43:55 +0000

    allow init project from command line

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-var generate = require('./lib/generator.js');
 var Logger = require('./lib/cli-logger.js');
+var logger = new Logger();
+switch(process.argv[2]){
+	case 'init':
+		var content = require('./lib/content');
+		content.setupDefault({}, function(err, result){
+			if(err) return logger.error(err);
 
-generate(new Logger(), function(err){
-	//callback	
-});
+			logger.log("finished init setup");
+		});
+		break;
+	case 'generate':
+	case undefined:
+		var generate = require('./lib/generator.js');
+		generate(logger, function(err){
+			//callback
+		});
+		break;
+	default:
+		logger.error(new Error(process.argv[2] + " is not a valid command"));
+		break;
+}
